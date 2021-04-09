4243bf6866db862f0d44e1007d22714dd82d02f4
Max Lyth <maxlyth@users.noreply.github.com>
2021-04-09 20:43:54 +0100

    Change deafault webGUI port

--- shelly-admin/rootfs/etc/cont-init.d/shelly-admin.sh
+++ shelly-admin/rootfs/etc/cont-init.d/shelly-admin.sh
@@ -3,17 +3,3 @@
 # Home Assistant Add-on: Shelly-Admin
 # Configures Shelly-Admin before running
 # ==============================================================================
-declare port
-
-# Pass in port & SSL settings
-port=$(bashio::addon.port 80)
-sed -i "s/3000/${port:-80}/" "/shelly-admin/bin/www"
-if ! bashio::var.has_value "${port}"; then
-    bashio::log.warning
-    bashio::log.warning "Direct access mode is disabled, Shelly Admin"
-    bashio::log.warning "will not work!"
-    bashio::log.warning
-    bashio::log.warning "Please assign a port in the Network section of this"
-    bashio::log.warning "add-on configuration."
-    bashio::log.warning
-fi
