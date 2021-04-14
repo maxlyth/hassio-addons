ce1f0b5db1f33409d2ddb1ceb26e61655f44c9cb
Max Lyth <maxlyth@users.noreply.github.com>
2021-04-14 17:33:24 +0100

    Fixes for NGinX proxy

--- shelly-admin/rootfs/etc/cont-init.d/nginx.sh
+++ shelly-admin/rootfs/etc/cont-init.d/nginx.sh
@@ -14,11 +14,14 @@ port "^$(bashio::addon.ingress_port)" \
 -out /etc/nginx/servers/ingress.conf
 
 # Generate direct access configuration, if enabled.
-admin_port=$(bashio::addon.port 8031)
+admin_port=$(bashio::addon.port 43812)
 if bashio::var.has_value "${admin_port}"; then
     bashio::config.require.ssl
     bashio::var.json \
+    certfile "$(bashio::config 'certfile')" \
+    keyfile "$(bashio::config 'keyfile')" \
     port "^${admin_port}" \
+    ssl "^$(bashio::config 'ssl')" \
     | tempio \
     -template /etc/nginx/templates/direct.gtpl \
     -out /etc/nginx/servers/direct.conf
--- shelly-admin/rootfs/etc/cont-init.d/shelly-admin.sh
+++ shelly-admin/rootfs/etc/cont-init.d/shelly-admin.sh
@@ -3,3 +3,20 @@
 # Home Assistant Add-on: Shelly-Admin
 # Configures Shelly-Admin before running
 # ==============================================================================
+declare port
+
+# Pass in port & SSL settings
+port=$(bashio::addon.port 43812)
+sed -i "s/43812/${port}/" "/shelly-admin/bin/www"
+
+if bashio::config.true 'ssl'; then
+    #    sed -i "s/%%SSL%%/true/" "/shelly-admin/bin/www"
+    bashio::log.warning
+    bashio::log.warning "Direct access to Shelly Admin via SSL is not yet available!"
+    bashio::log.warning
+    bashio::log.warning "Please remove the port assignment and allow Home Assistant"
+    bashio::log.warning "to handle ingress."
+    bashio::log.warning
+else
+    #    sed -i "s/%%SSL%%/false/" "/shelly-admin/bin/www"
+fi
