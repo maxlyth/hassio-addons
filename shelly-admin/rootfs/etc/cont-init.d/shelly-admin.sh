5cbc5e4ebebce10d106edbc8f20215e19d343eed
Max Lyth <maxlyth@users.noreply.github.com>
2021-04-15 10:51:04 +0100

    v0.1.6 remove proxy

--- shelly-admin/rootfs/etc/cont-init.d/shelly-admin.sh
+++ shelly-admin/rootfs/etc/cont-init.d/shelly-admin.sh
@@ -6,17 +6,17 @@
 declare port
 
 # Pass in port & SSL settings
-port=$(bashio::addon.port 43812)
+port=$(bashio::addon.ingress_port)
 sed -i "s/43812/${port}/" "/shelly-admin/bin/www"
 
-if bashio::config.true 'ssl'; then
-    #    sed -i "s/%%SSL%%/true/" "/shelly-admin/bin/www"
-    bashio::log.warning
-    bashio::log.warning "Direct access to Shelly Admin via SSL is not yet available!"
-    bashio::log.warning
-    bashio::log.warning "Please remove the port assignment and allow Home Assistant"
-    bashio::log.warning "to handle ingress."
-    bashio::log.warning
-    #else
-    #    sed -i "s/%%SSL%%/false/" "/shelly-admin/bin/www"
-fi
+#if bashio::config.true 'ssl'; then
+#    #    sed -i "s/%%SSL%%/true/" "/shelly-admin/bin/www"
+#    bashio::log.warning
+#    bashio::log.warning "Direct access to Shelly Admin via SSL is not yet available!"
+#    bashio::log.warning
+#    bashio::log.warning "Please remove the port assignment and allow Home Assistant"
+#    bashio::log.warning "to handle ingress."
+#    bashio::log.warning
+#    #else
+#    #    sed -i "s/%%SSL%%/false/" "/shelly-admin/bin/www"
+#fi
