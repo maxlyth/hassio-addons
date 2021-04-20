c7d8103fd75281eee2a3747aea33485e94fa57ca
Max Lyth <maxlyth@users.noreply.github.com>
2021-04-20 23:29:29 +0100

    v0.3.0

--- shelly-admin/rootfs/run.sh
+++ shelly-admin/rootfs/run.sh
@@ -3,19 +3,19 @@
 # Home Assistant  Add-on: Shelly-Admin
 # Starts Shelly-Admin
 # ==============================================================================
-declare port
-declare host
-declare prefix
 
 # Pass in port, host and URL settings
 bashio::log.info $(bashio::addon.ingress_port)
 bashio::log.info $(bashio::addon.ingress_url)
+bashio::log.info $(bashio::config.shelly_auth.username)
 bashio::log.info $(bashio::addon.ip_address)
 bashio::log.info $(bashio::addon.network)
 
 port=$(bashio::addon.ingress_port)
 host="0.0.0.0"
 prefix=""
+shellyuser=$(bashio::config.shelly_auth.username)
+shellypw=$(bashio::config.shelly_auth.password)
 
 export NODE_PATH=/shelly-admin/node_modules
 
@@ -27,6 +27,8 @@ PORT=${port}
 PREFIX=${prefix}
 TRUSTPROXY=loopback, 172.30.32.1
 UIMODE=light
+SHELLYUSER=${shellyuser}
+SHELLYPW=${shellypw}
 EOL
 
 bashio::log.info "Starting Shelly-Admin..."
